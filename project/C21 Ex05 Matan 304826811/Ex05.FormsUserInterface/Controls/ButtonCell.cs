1276156c794988c71ccd68de428f3a497143b03b
Matan <matans844@gmail.com>
2021-09-19 16:40:29 +0300

    Added skeleton for button to Game Cell.

---
+++ project/C21 Ex05 Matan 304826811/Ex05.FormsUserInterface/Controls/ButtonCell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Ex05.FormsUserInterface
+{
+	public sealed class ButtonCell : Button
+	{
+	}
+}
